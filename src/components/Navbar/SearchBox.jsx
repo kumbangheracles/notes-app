40fc1a1ddcfc9fd18a1d6f16ddbfca4a427ed0b4
Kumbang heracles <153085696+kumbangheracles@users.noreply.github.com>
2024-12-01 13:09:17 +0700

    Update SearchBox.jsx

--- src/components/Navbar/SearchBox.jsx
+++ src/components/Navbar/SearchBox.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 
-const SearchBox = ({ searchQuery, onSearch }) => {
-    return (
-      <input
-        type="text"
-        placeholder="Cari catatan..."
-        value={searchQuery}
-        onChange={(e) => onSearch(e.target.value)}
-        className="search-box"
-      />
-    );
-  };
-
+function SearchBox(){
+  return(
+    <input type="text" placeholder="Cari catatan..." className="search-box"/>
+  )
+}
   export default SearchBox;
